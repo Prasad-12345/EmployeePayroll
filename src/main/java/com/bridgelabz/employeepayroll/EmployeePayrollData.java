2c780c006a210bce93ed3b1cb8309bdc00f8cd6e
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-10 16:39:57 +0530

    Added: Employee data

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollData.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayrollData {
+    public String empId;
+    public String empName;
+    public double empSalary;
+
+    public EmployeePayrollData(String empId, String empName, double empSalary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empSalary = empSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "empId='" + empId + '\'' +
+                ", empName='" + empName + '\'' +
+                ", empSalary=" + empSalary +
+                '}';
+    }
+}
