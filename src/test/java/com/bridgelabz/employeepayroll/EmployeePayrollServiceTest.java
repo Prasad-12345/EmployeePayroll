cf43af47588e1097638417eb207886dcf52fb1ef
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-17 16:03:11 +0530

    Added: Create an Employee Payroll Service to store Employee Payroll into a File

---
+++ src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.employeepayroll;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    /*
+     *Test case to ensure writeData method
+     */
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() throws IOException {
+        EmployeePayrollData[] arraysOfEmp = {
+                new EmployeePayrollData("1", "Prasad", 600000 ),
+                new EmployeePayrollData("2", "Vipul", 700000 ),
+                new EmployeePayrollData("3", "Swapnil", 800000)
+        };
+
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arraysOfEmp));
+        employeePayrollService.writeEmpPayrollData(EmployeePayrollService.IOService.FILE_IO);
+
+        EmployeePayrollFileIOService obj = new EmployeePayrollFileIOService();
+        long entries = obj.entries();
+        Assert.assertEquals(entries, 3);
+    }
+}
